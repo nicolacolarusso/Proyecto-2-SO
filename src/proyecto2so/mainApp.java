6b891701eab9f58255e99e553d3937e45ff19aba
Nicola Colarusso <131668387+nicolacolarusso@users.noreply.github.com>
2024-10-15 19:41:02 -0400

    Craeción proyecto

---
+++ src/proyecto2so/mainApp.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package proyecto2so;
+
+/**
+ *
+ * @author nicolagabrielecolarusso
+ */
+public class mainApp {
+    
+    public static void main(String[] args) {
+     
+        //App app = new App();
+        //app.start();
+    }
+}
